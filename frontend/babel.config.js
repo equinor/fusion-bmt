6ad1191f2134f2c9dc3ebed24149bd44bdeddb10
Nicholas Dalhaug <nicd@equinor.com>
2020-11-03 09:18:14 +0100

    Enable tests for react and typescript

---
+++ frontend/babel.config.js
@@ -0,0 +1,19 @@
+module.exports = function babelConfig(api) {
+    api.cache(true)
+
+    const presets = ['@babel/preset-env', '@babel/preset-react']
+
+    const env = {
+      test: {
+        presets: [
+          ['@babel/preset-env', { targets: { node: 'current' } }],
+          '@babel/preset-react',
+        ],
+      },
+    }
+
+    return {
+      presets,
+      env,
+    }
+  }
