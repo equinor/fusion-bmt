c06ac0d65c94b32be3a3e4f44c09205c99ca189b
Nicholas Dalhaug <nicd@equinor.com>
2020-11-03 12:27:23 +0100

    Test Q&A component

---
+++ frontend/src/components/QuestionAndAnswer/AnswerSeverityForm.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { cleanup, fireEvent } from '@testing-library/react'
+import AnswerSeverityForm from './AnswerSeverityForm'
+import { Severity } from '../../api/models'
+import { renderWithContext } from '../../utils/FusionTestWrapper'
+
+afterEach(cleanup)
+
+describe('AnswerSeverityForm', () => {
+    it('Call on click', () => {
+        const onSeveritySelected = jest.fn()
+
+        const {getByLabelText} = renderWithContext(
+            <AnswerSeverityForm
+                severity={Severity.HIGH}
+                onSeveritySelected={onSeveritySelected}
+            />
+        )
+
+        const severityFormRadio = getByLabelText('Limited')
+
+        fireEvent.click(severityFormRadio)
+
+        expect(onSeveritySelected).toHaveBeenCalledTimes(1)
+    })
+})
