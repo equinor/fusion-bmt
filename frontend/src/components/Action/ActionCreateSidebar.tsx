fb0d17ae2e3e84d4a329225accd87d71a9920f4f
Nicholas Dalhaug <nicd@equinor.com>
2020-12-15 15:53:07 +0100

    Add a sidebar to create actions
    Check is loading participants in action sidebar better

---
+++ frontend/src/components/Action/ActionCreateSidebar.tsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react'
+
+import { PersonDetails, useApiClients } from '@equinor/fusion'
+import { ModalSideSheet } from '@equinor/fusion-components'
+
+import { Action, Participant, Question } from '../../api/models'
+import ActionCreateForm from './ActionCreateForm'
+import { CircularProgress } from '@equinor/eds-core-react'
+
+interface Props {
+    open: boolean
+    connectedQuestion: Question
+    possibleAssignees: Participant[]
+    onActionCreate: (action: Action) => void
+    onCloseClick: () => void
+}
+
+const ActionCreateSidebar = ({ open, connectedQuestion, possibleAssignees, onActionCreate, onCloseClick }: Props) => {
+    const apiClients = useApiClients()
+    const [personDetailsList, setPersonDetailsList] = useState<PersonDetails[]>([])
+
+    const isLoading = personDetailsList.length !== possibleAssignees.length
+
+    const getAllPersonDetails = (azureUniqueIds: string[]): Promise<PersonDetails[]> => {
+        const manyPrommises: Promise<PersonDetails>[] = azureUniqueIds.map(azureUniqueId => {
+            return apiClients.people.getPersonDetailsAsync(azureUniqueId).then(response => {
+                return response.data
+            })
+        })
+
+        return Promise.all(manyPrommises)
+    }
+
+    useEffect(() => {
+        let shouldUpdate = true
+
+        const azureUniqueIds = possibleAssignees.map(a => a.azureUniqueId)
+        getAllPersonDetails(azureUniqueIds).then(fetchedPersonDetailsList => {
+            if(shouldUpdate){
+                setPersonDetailsList(fetchedPersonDetailsList)
+            }
+        })
+
+        return () => {shouldUpdate = false}
+    }, [possibleAssignees])
+
+    return (
+        <ModalSideSheet
+            header='Add Action'
+            show={open}
+            size='large'
+            onClose={onCloseClick}
+            isResizable={false}
+        >
+            {isLoading &&
+                <div style={{textAlign: "center"}}>
+                    <CircularProgress />
+                </div>
+            }
+            {!isLoading &&
+                <div style={{margin: 20}}>
+                    <ActionCreateForm
+                        connectedQuestion={connectedQuestion}
+                        possibleAssignees={possibleAssignees}
+                        possibleAssigneesDetails={personDetailsList}
+                        onActionCreate={onActionCreate}
+                        onCancelClick={onCloseClick}
+                    />
+                </div>
+            }
+        </ModalSideSheet>
+    )
+}
+
+export default ActionCreateSidebar
--- frontend/src/views/Evaluation/EvaluationGQL.ts
+++ frontend/src/views/Evaluation/EvaluationGQL.ts
@@ -111,6 +111,9 @@ export const useEvaluationQuery = (evaluationId: string): EvaluationQueryProps =
                     answers {
                         ...AnswerFields
                     }
+                    evaluation {
+                        ...EvaluationFields
+                    }
                 }
             }
         }
