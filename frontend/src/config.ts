18bfa9abaf2c0b9b7b1fa5d408f3d97afc214228
Kristian Reed <krr@equinor.com>
2020-12-21 18:08:42 +0100

    Hard code backend url for compatibility

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,17 +1,9 @@
-declare const API_URL: string
-declare const APP_VERSION: string
-declare const COMMITHASH: string
-declare const BRANCH: string
-declare const LAST_COMMIT: string
-declare const AD_APP_ID: string
-declare const APP_INSIGHTS: string
+let API_URL: string = 'https://backend-fusion-bmt-dev.radix.equinor.com'
+let AD_APP_ID: string = '8829d4ca-93e8-499a-8ce1-bc0ef4840176'
+let APP_INSIGHTS: string = '6f08fb54-d348-44cb-9fa6-be2c9e29d419'
 
 export const config = {
     API_URL: API_URL,
-    APP_VERSION: APP_VERSION,
-    COMMITHASH: COMMITHASH,
-    BRANCH: BRANCH,
-    LAST_COMMIT: LAST_COMMIT,
     AD_APP_ID: AD_APP_ID,
     APP_INSIGHTS: APP_INSIGHTS
 }
