573f86660a1d4e6b1db5757a4ca026b90e13d248
Kristian Reed <krr@equinor.com>
2021-01-05 19:09:09 +0100

    Fix deploy to fusion CI

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,17 +1,9 @@
 declare const API_URL: string
-declare const APP_VERSION: string
-declare const COMMITHASH: string
-declare const BRANCH: string
-declare const LAST_COMMIT: string
 declare const AD_APP_ID: string
 declare const APP_INSIGHTS: string
 
 export const config = {
     API_URL: API_URL,
-    APP_VERSION: APP_VERSION,
-    COMMITHASH: COMMITHASH,
-    BRANCH: BRANCH,
-    LAST_COMMIT: LAST_COMMIT,
     AD_APP_ID: AD_APP_ID,
     APP_INSIGHTS: APP_INSIGHTS
 }
