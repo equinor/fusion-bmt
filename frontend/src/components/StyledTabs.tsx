bf5367f74ab162b35d5eb718e041c28ee30d065a
Nicholas Dalhaug <nicd@equinor.com>
2021-01-15 09:48:41 +0100

    Make styled tabs reusable

---
+++ frontend/src/components/StyledTabs.tsx
@@ -0,0 +1,9 @@
+import { Tabs } from '@equinor/eds-core-react'
+import styled from 'styled-components'
+
+const { TabPanel } = Tabs
+
+export const StyledTabPanel = styled(TabPanel)`
+    padding-top: 0px;
+    border-top: 1px solid LightGray;
+`
