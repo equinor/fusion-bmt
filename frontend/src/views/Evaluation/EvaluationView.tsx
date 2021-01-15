734bed91d6d7e54261d5b90378010580de9c991c
Nicholas Dalhaug <nicd@equinor.com>
2021-01-15 09:51:20 +0100

    See facilitator progress in Preparation

--- frontend/src/views/Evaluation/EvaluationView.tsx
+++ frontend/src/views/Evaluation/EvaluationView.tsx
@@ -16,78 +16,75 @@ interface EvaluationViewProps {
     onProgressParticipant: (newProgressions: Progression) => void
 }
 
-const EvaluationView = ({evaluation, onProgressEvaluationClick, onProgressParticipant}: EvaluationViewProps) => {
-    const allowedRoles = Object.values(Role)
-    return <>
-        <Stepper
-            forceOrder={false}
-            activeStepKey={evaluation.progression}
-            hideNavButtons={true}
-        >
-            <Step
-                title={progressionToString(Progression.Nomination)}
-                description={calcProgressionStatus(evaluation.progression, Progression.Nomination)}
-                stepKey={Progression.Nomination}
-            >
-                <NominationView
-                    evaluation={evaluation}
-                    onNextStep={() => onProgressEvaluationClick()}
-                />
-            </Step>
-            <Step
-                title={progressionToString(Progression.Individual)}
-                description={calcProgressionStatus(evaluation.progression, Progression.Individual)}
-                stepKey={Progression.Individual}
-            >
-                <QuestionnaireStatusTabs evaluation={evaluation} viewProgression={Progression.Individual} allowedRoles={allowedRoles}>
-                    <IndividualView
+const EvaluationView = ({ evaluation, onProgressEvaluationClick, onProgressParticipant }: EvaluationViewProps) => {
+    const allRoles = Object.values(Role)
+    return (
+        <>
+            <Stepper forceOrder={false} activeStepKey={evaluation.progression} hideNavButtons={true}>
+                <Step
+                    title={progressionToString(Progression.Nomination)}
+                    description={calcProgressionStatus(evaluation.progression, Progression.Nomination)}
+                    stepKey={Progression.Nomination}
+                >
+                    <NominationView evaluation={evaluation} onNextStep={() => onProgressEvaluationClick()} />
+                </Step>
+                <Step
+                    title={progressionToString(Progression.Individual)}
+                    description={calcProgressionStatus(evaluation.progression, Progression.Individual)}
+                    stepKey={Progression.Individual}
+                >
+                    <QuestionnaireStatusTabs evaluation={evaluation} viewProgression={Progression.Individual} allowedRoles={allRoles}>
+                        <IndividualView
+                            evaluation={evaluation}
+                            onNextStepClick={() => onProgressEvaluationClick()}
+                            onProgressParticipant={onProgressParticipant}
+                        />
+                    </QuestionnaireStatusTabs>
+                </Step>
+                <Step
+                    title={progressionToString(Progression.Preparation)}
+                    description={calcProgressionStatus(evaluation.progression, Progression.Preparation)}
+                    stepKey={Progression.Preparation}
+                >
+                    <QuestionnaireStatusTabs
                         evaluation={evaluation}
-                        onNextStepClick={() => onProgressEvaluationClick()}
-                        onProgressParticipant={onProgressParticipant}
-                    />
-                </QuestionnaireStatusTabs>
-            </Step>
-            <Step
-                title={progressionToString(Progression.Preparation)}
-                description={calcProgressionStatus(evaluation.progression, Progression.Preparation)}
-                stepKey={Progression.Preparation}
-            >
-                <QuestionnaireStatusTabs evaluation={evaluation} viewProgression={Progression.Preparation} allowedRoles={[Role.OrganizationLead]}>
-                    <PreparationView
-                        evaluation={evaluation}
-                        onNextStepClick={() => onProgressEvaluationClick()}
-                        onProgressParticipant={onProgressParticipant}
-                    />
-                </QuestionnaireStatusTabs>
-            </Step>
-            <Step
-                title={progressionToString(Progression.Workshop)}
-                description={calcProgressionStatus(evaluation.progression, Progression.Workshop)}
-                stepKey={Progression.Workshop}
-            >
-                <div>
-                    {/* Div added to make position: sticky work for deeper components */}
-                    <WorkshopView
-                        evaluation={evaluation}
-                        onNextStepClick={() => onProgressEvaluationClick()}
-                        onProgressParticipant={onProgressParticipant}
-                    />
-                </div>
-            </Step>
-            <Step
-                title={progressionToString(Progression.FollowUp)}
-                description={calcProgressionStatus(evaluation.progression, Progression.FollowUp)}
-                stepKey={Progression.FollowUp}
-            >
-                <div>
-                    {/* Div added to make position: sticky work for deeper components */}
-                    <FollowUpView
-                        evaluation={evaluation}
-                    />
-                </div>
-            </Step>
-        </Stepper>
-    </>
+                        viewProgression={Progression.Preparation}
+                        allowedRoles={[Role.OrganizationLead, Role.Facilitator]}
+                    >
+                        <PreparationView
+                            evaluation={evaluation}
+                            onNextStepClick={() => onProgressEvaluationClick()}
+                            onProgressParticipant={onProgressParticipant}
+                        />
+                    </QuestionnaireStatusTabs>
+                </Step>
+                <Step
+                    title={progressionToString(Progression.Workshop)}
+                    description={calcProgressionStatus(evaluation.progression, Progression.Workshop)}
+                    stepKey={Progression.Workshop}
+                >
+                    <div>
+                        {/* Div added to make position: sticky work for deeper components */}
+                        <WorkshopView
+                            evaluation={evaluation}
+                            onNextStepClick={() => onProgressEvaluationClick()}
+                            onProgressParticipant={onProgressParticipant}
+                        />
+                    </div>
+                </Step>
+                <Step
+                    title={progressionToString(Progression.FollowUp)}
+                    description={calcProgressionStatus(evaluation.progression, Progression.FollowUp)}
+                    stepKey={Progression.FollowUp}
+                >
+                    <div>
+                        {/* Div added to make position: sticky work for deeper components */}
+                        <FollowUpView evaluation={evaluation} />
+                    </div>
+                </Step>
+            </Stepper>
+        </>
+    )
 }
 
 export default EvaluationView
