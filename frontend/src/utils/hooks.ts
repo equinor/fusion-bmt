1568ea15660897d31fd4f06d93a37db95ef9da4b
Nicholas Dalhaug <nicd@equinor.com>
2021-01-20 11:17:04 +0100

    Create use effect without mount run

---
+++ frontend/src/utils/hooks.ts
@@ -0,0 +1,12 @@
+import { useEffect, useRef } from "react"
+
+export const useEffectNotOnMount = (f: () => void, deps: any[]) => {
+    const firstUpdate = useRef(true)
+    return useEffect(() => {
+        if (firstUpdate.current === true) {
+            firstUpdate.current = false
+            return
+        }
+        return f()
+    }, deps)
+}
