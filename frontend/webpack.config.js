8c4a05491cfefcd7ff4a1f48a785b9c268fab786
Kristian Reed <krr@equinor.com>
2021-01-29 15:22:49 +0100

    Fix Application Insight env variable

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,17 +1,16 @@
 const webpack = require('webpack')
 
-
 let isProduction = false
-if (process.env.BUILD_MODE && process.env.BUILD_MODE === "production") {
+if (process.env.BUILD_MODE && process.env.BUILD_MODE === 'production') {
     isProduction = true
 }
 const mode = isProduction ? 'production' : 'development'
 
 const API_URL = process.env.API_URL || 'http://localhost:5000'
 const AD_APP_ID = process.env.AD_CLIENT_ID || '8829d4ca-93e8-499a-8ce1-bc0ef4840176'
-const APP_INSIGHTS = process.env.ApplicationInsights__InstrumentationKey || ''
+const APP_INSIGHTS_KEY = process.env.APP_INSIGHTS_KEY || ''
 
-console.log("--- MODE:", mode, "API_URL:", API_URL)
+console.log('--- MODE:', mode, 'API_URL:', API_URL)
 
 module.exports = {
     mode: mode,
@@ -19,7 +18,7 @@ module.exports = {
         new webpack.DefinePlugin({
             API_URL: JSON.stringify(API_URL),
             AD_APP_ID: JSON.stringify(AD_APP_ID),
-            APP_INSIGHTS: JSON.stringify(APP_INSIGHTS)
-        })
-    ]
+            APP_INSIGHTS: JSON.stringify(APP_INSIGHTS_KEY),
+        }),
+    ],
 }
