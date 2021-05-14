4c963642a08d02f537518a405a0206dd1a09ec2c
Kristian Reed <krr@equinor.com>
2021-05-14 14:56:46 +0200

    Update frontend dependencies

--- frontend/src/utils/FusionTestWrapper.tsx
+++ frontend/src/utils/FusionTestWrapper.tsx
@@ -4,20 +4,21 @@ import React from 'react'
 import { HashRouter } from 'react-router-dom'
 
 const serviceResolver: ServiceResolver = {
+    getContextBaseUrl: () => 'https://pro-s-context-ci.azurewebsites.net',
     getDataProxyBaseUrl: () => 'https://pro-s-dataproxy-ci.azurewebsites.net',
     getFusionBaseUrl: () => 'https://pro-s-portal-ci.azurewebsites.net',
-    getContextBaseUrl: () => 'https://pro-s-context-ci.azurewebsites.net',
+    getMeetingsBaseUrl: () => 'https://pro-s-meeting-v2-ci.azurewebsites.net',
     getOrgBaseUrl: () => 'https://pro-s-org-ci.azurewebsites.net',
     getPowerBiBaseUrl: () => 'https://pro-s-powerbi-ci.azurewebsites.net',
-    getTasksBaseUrl: () => 'https://pro-s-tasks-ci.azurewebsites.net',
     getProjectsBaseUrl: () => 'https://pro-s-projects-ci.azurewebsites.net',
-    getMeetingsBaseUrl: () => 'https://pro-s-meetingsv2-ci.azurewebsites.net',
+    getTasksBaseUrl: () => 'https://pro-s-tasks-ci.azurewebsites.net',
     getPeopleBaseUrl: () => 'https://pro-s-people-ci.azurewebsites.net',
     getReportsBaseUrl: () => 'https://pro-s-reports-ci.azurewebsites.net',
     getPowerBiApiBaseUrl: () => 'https://api.powerbi.com/v1.0/myorg',
     getNotificationBaseUrl: () => 'https://pro-s-notification-ci.azurewebsites.net',
-    getInfoUrl: () => 'https://pro-s-info-app-CI.azurewebsites.net',
+    getInfoUrl: () => 'https://pro-s-info-app-ci.azurewebsites.net',
     getFusionTasksBaseUrl: () => 'https://pro-s-tasks-ci.azurewebsites.net',
+    getBookmarksBaseUrl: () => 'https://pro-s-bookmarks-ci.azurewebsites.net',
 }
 
 export const FusionTestWrapper: React.FC = ({ children }) => {
