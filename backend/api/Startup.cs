6808211ceddb0e36ed00bb0a74cc000dc65fd18f
Alena Chaikouskaya <alec@equinor.com>
2021-07-01 20:02:06 +0200

    Set up mock authentication

--- backend/api/Startup.cs
+++ backend/api/Startup.cs
@@ -175,9 +175,9 @@ namespace api
                 logger.LogInformation("Using Sqlite in memory database");
             }
 
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.EnvironmentName == "Test")
             {
-                logger.LogInformation("Configuring for development environment");
+                logger.LogInformation($"Configuring for {env.EnvironmentName} environment");
                 app.UseDeveloperExceptionPage();
 
                 var option = new RewriteOptions();
--- frontend/cypress/integration/sample_spec.ts
+++ frontend/cypress/integration/sample_spec.ts
@@ -1,3 +1,10 @@
+// TODO: login stuff: where, what, how, etc
+beforeEach(() => {
+    cy.login()
+    const port = Cypress.env('FRONTEND_PORT') || '3000'
+    cy.visit(`http://localhost:${port}/123`)
+})
+
 describe('Sample Test', () => {
     it('Works', () => {
         expect(true).to.equal(true)
