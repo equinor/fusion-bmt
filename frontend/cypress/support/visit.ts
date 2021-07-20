4ff759368babde234c26ddb390496ee18ce96452
Alena Chaikouskaya <alec@equinor.com>
2021-07-20 17:45:37 +0200

    Navigate to project page before evaluation
    Explicit navigation to project prevents "Please select a project" issue
    on first run.

--- frontend/cypress/support/visit.ts
+++ frontend/cypress/support/visit.ts
@@ -4,6 +4,7 @@ import users, { User } from './users'
 Cypress.Commands.add('visitEvaluation', (evaluationId: string, user: User=users[0]) => {
     cy.login(user)
     const port = Cypress.env('FRONTEND_PORT') || '3000'
+    cy.visit(`http://localhost:${port}/123`)
     cy.visit(`http://localhost:${port}/123/evaluation/${evaluationId}`)
     cy.wait(1000) // A poor solution tbh
 })
