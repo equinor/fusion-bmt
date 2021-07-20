29499508b4b3e9aef763c69d8eac42561d829d9f
Alena Chaikouskaya <alec@equinor.com>
2021-07-20 12:28:37 +0200

    Add Delete Action functionality

--- frontend/src/api/mutations.ts
+++ frontend/src/api/mutations.ts
@@ -150,6 +150,59 @@ export const useEditActionMutation = (): EditActionMutationProps => {
     }
 }
 
+interface DeleteActionMutationProps {
+    deleteAction: (actionId: string) => void
+    loading: boolean
+    action: Action | undefined
+    error: ApolloError | undefined
+}
+
+/**
+ * Removes the action from view.
+ * Note that cache update is configured to only deal with situation where
+ * action is loaded via question.
+ *
+ * @param questionId Id of question action is assigned to
+ */
+export const useDeleteActionMutation = (questionId: string): DeleteActionMutationProps => {
+    const DELETE_ACTION = gql`
+        mutation DeleteAction($actionId: String) {
+            deleteAction(actionId: $actionId) {
+                id
+            }
+        }
+    `
+    const [deleteActionApolloFunc, { loading, data, error }] = useMutation(DELETE_ACTION, {
+        update(cache, mutationResult) {
+            const deleteAction = mutationResult.data.deleteAction
+            cache.modify({
+                id: cache.identify({
+                    __typename: 'Question',
+                    id: questionId,
+                }),
+                fields: {
+                    actions(existingActionRefs, { readField }) {
+                        return existingActionRefs.filter((actionRef: any) => {
+                            return deleteAction.id !== readField('id', actionRef)
+                        })
+                    },
+                },
+            })
+        },
+    })
+
+    const deleteAction = (actionId: string) => {
+        deleteActionApolloFunc({ variables: { actionId } })
+    }
+
+    return {
+        deleteAction,
+        loading,
+        action: data?.deleteAction,
+        error,
+    }
+}
+
 interface CreateNoteMutationProps {
     createNote: (actionId: string, text: string) => void
     loading: boolean
