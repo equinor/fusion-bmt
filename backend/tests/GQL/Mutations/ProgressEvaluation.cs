e72da853b71f1ee9d40f58f37340b74687946c19
Erlend Hårstad <erha@equinor.com>
2021-09-03 16:13:18 +0200

    Add backend auth-tests for ProgressEvaluation

---
+++ backend/tests/GQL/Mutations/ProgressEvaluation.cs
@@ -0,0 +1,94 @@
+using Xunit;
+using System;
+using System.Linq;
+
+using api.Models;
+using api.Services;
+using api.GQL;
+
+
+namespace tests
+{
+    public class ProgressEvaluationMutation : MutationTest
+    {
+        private readonly Evaluation _evaluation;
+        private readonly Participant _facilitator;
+        private readonly Participant _organizationLead;
+        private readonly Participant _participant;
+        private readonly Participant _readonly;
+
+        public ProgressEvaluationMutation() {
+            _evaluation = CreateEvaluation();
+            _facilitator = _evaluation.Participants.First();
+            _authService.LoginUser(_facilitator);
+
+            _organizationLead = CreateParticipant(_evaluation, role: Role.OrganizationLead);
+            _participant = CreateParticipant(_evaluation, role: Role.Participant);
+            _readonly = CreateParticipant(_evaluation, role: Role.ReadOnly);
+        }
+
+        /* Tests */
+
+        [Fact]
+        public void FacilitatorCanUseMutation()
+        {
+            AssertCanProgress(_facilitator);
+        }
+
+        [Fact]
+        public void OrganizationLeadIsUnauthorized()
+        {
+            AssertIsNotAuthorized(_organizationLead);
+        }
+
+        [Fact]
+        public void ParticipantIsUnauthorized()
+        {
+            AssertIsNotAuthorized(_participant);
+        }
+
+        [Fact]
+        public void ReadOnlyIsUnauthorized()
+        {
+            AssertIsNotAuthorized(_readonly);
+        }
+
+        [Fact]
+        public void NonParcipantIsUnauthorized()
+        {
+            string AzureUniqueId = Randomize.Integer().ToString();
+            AssertIsNotAuthorized(AzureUniqueId);
+        }
+
+        /* Helper methods */
+
+        private void AssertCanProgress(Participant user)
+        {
+            _authService.LoginUser(user);
+            Progression newProgression = Randomize.Progression();
+
+            _mutation.ProgressEvaluation(
+                evaluationId: _evaluation.Id,
+                newProgression: newProgression
+            );
+
+            Assert.True(_evaluation.Progression == newProgression);
+        }
+
+        private void AssertIsNotAuthorized(string azureUniqueId)
+        {
+            _authService.LoginUser(azureUniqueId);
+            Assert.Throws<UnauthorizedAccessException>(() =>
+                _mutation.ProgressEvaluation(
+                    evaluationId: _evaluation.Id,
+                    newProgression: Randomize.Progression()
+                )
+            );
+        }
+
+        private void AssertIsNotAuthorized(Participant user)
+        {
+            AssertIsNotAuthorized(user.AzureUniqueId);
+        }
+    }
+}
