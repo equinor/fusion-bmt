c8c4b30eda2f382eab3817c471e1967969af32fa
hnformentin <helena.formentin@gmail.com>
2021-09-15 11:46:43 +0200

    Restructure tests into Evaluation Management description

--- frontend/cypress/integration/evaluation_spec.ts
+++ frontend/cypress/integration/evaluation_spec.ts
@@ -3,51 +3,52 @@ import { EvaluationSeed } from '../support/evaluation_seed'
 import { evaluationName } from '../support/helpers'
 import NominationPage from '../support/nomination'
 import ProjectPage from '../support/project'
-import { users, User } from '../support/mock/external/users'
+import { getUsers, users, User } from '../support/mock/external/users'
 import * as faker from 'faker'
+import { EvaluationPage } from '../support/evaluation'
 
-const createEvaluation = (creator: User, otherUser: User, roles: Role[], prefix: string) => {
-    let seed = new EvaluationSeed({
-        progression: faker.random.arrayElement(Object.values(Progression)),
-        users: [creator, otherUser],
-        roles,
-        namePrefix: prefix,
-    })
-    return seed
-}
+describe('Evaluation management', () => {
+    const createEvaluation = (creator: User, otherUser: User, roles: Role[], prefix: string) => {
+        let seed = new EvaluationSeed({
+            progression: faker.random.arrayElement(Object.values(Progression)),
+            users: [creator, otherUser],
+            roles,
+            namePrefix: prefix,
+        })
+        return seed
+    }
 
-describe('Creating a new Evaluation', () => {
-    const user = users[2]
-    const roles = [Role.Facilitator, Role.Participant]
-    const evalUserIsFacilitator = createEvaluation(user, users[0], roles, 'user is Facilitator')
-    const evalUserIsParticipant = createEvaluation(users[0], user, roles, 'user is Participant')
-    const evalUserIsNotInEvaluation = createEvaluation(users[0], users[1], roles, 'user is not in evaluation')
-    const previousEvaluations = [evalUserIsFacilitator, evalUserIsParticipant, evalUserIsNotInEvaluation]
+    context('Creating a new Evaluation', () => {
+        const user = users[2]
+        const roles = [Role.Facilitator, Role.Participant]
+        const evalUserIsFacilitator = createEvaluation(user, users[0], roles, 'user is Facilitator')
+        const evalUserIsParticipant = createEvaluation(users[0], user, roles, 'user is Participant')
+        const evalUserIsNotInEvaluation = createEvaluation(users[0], users[1], roles, 'user is not in evaluation')
+        const previousEvaluations = [evalUserIsFacilitator, evalUserIsParticipant, evalUserIsNotInEvaluation]
 
-    before(() => {
-        evalUserIsFacilitator.plant()
-        evalUserIsParticipant.plant()
-        evalUserIsNotInEvaluation.plant()
-    })
+        before(() => {
+            evalUserIsFacilitator.plant()
+            evalUserIsParticipant.plant()
+            evalUserIsNotInEvaluation.plant()
+        })
 
-    beforeEach(() => {
-        cy.visitProject(user)
-    })
+        beforeEach(() => {
+            cy.visitProject(user)
+        })
 
-    it('Without setting a previous evaluation', () => {
-        const name = evaluationName({ prefix: 'CreatedWithoutPrevLink' })
+        it('Without setting a previous evaluation', () => {
+            const name = evaluationName({ prefix: 'CreatedWithoutPrevLink' })
 
-        const projectPage = new ProjectPage()
-        projectPage.createEvaluationButton().click()
+            const projectPage = new ProjectPage()
+            projectPage.createEvaluationButton().click()
 
-        const dialog = new ProjectPage.CreateEvaluationDialog()
-        dialog.createEvaluation(name, 'SquareField')
+            const dialog = new ProjectPage.CreateEvaluationDialog()
+            dialog.createEvaluation(name, 'SquareField')
 
-        const nominationPage = new NominationPage()
-        nominationPage.evaluationTitle().should('have.text', name)
-    })
+            const nominationPage = new NominationPage()
+            nominationPage.evaluationTitle().should('have.text', name)
+        })
 
-    context('Can create evaluation ', () => {
         previousEvaluations.forEach(previous => {
             it(`Choosing a previous evaluation where ${previous.name}`, () => {
                 const name = evaluationName({ prefix: 'CreatedWithPrevLink' })
