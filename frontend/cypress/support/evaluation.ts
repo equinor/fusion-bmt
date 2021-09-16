fa13d29b91301d433af0b472c9b659e95df0d2dd
hnformentin <helena.formentin@gmail.com>
2021-09-16 09:06:43 +0200

    Add state as an option in progressionStepLink

--- frontend/cypress/support/evaluation.ts
+++ frontend/cypress/support/evaluation.ts
@@ -2,14 +2,14 @@ import { Progression } from '../../src/api/models'
 import { progressionToString } from '../../src/utils/EnumToString'
 
 export class EvaluationPage {
-    progressionStepLink = (progression: Progression) => {
+    progressionStepLink = (progression: Progression, state: 'In Progress' | 'Complete' | 'Awaiting' | '' = '') => {
         /**
          * We can't identify stepper itself, without stepper content.
          * We also have no ids to tie up to. Thus at the moment search
          * the whole document for links with expected progression text.
          * Seems to hold as of now.
          */
-        return cy.contains('a', progressionToString(progression))
+        return cy.contains('a', new RegExp(`${progressionToString(progression)}.*${state}`))
     }
 }
 
