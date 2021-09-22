c96904923e6dde41f774a83dac2f80f1f04ca54f
Nina Andersen <denektenina@gmail.com>
2021-09-22 12:24:47 +0200

    Added tests for edit question template

--- frontend/cypress/integration/admin_page_spec.ts
+++ frontend/cypress/integration/admin_page_spec.ts
@@ -1,17 +1,52 @@
 import { users } from '../support/mock/external/users'
- 
+import * as faker from 'faker'
+import { Organization } from '../../src/api/models'
+
 describe('Admin page', () => {
-    before(() => {
+    beforeEach(() => {
         const user = users[users.length - 1]
         cy.visitProject(user)
+        cy.get('[id^=question-]').as('qs')
     })
 
-    it('Navigate to Admin page', () => {
-        cy.get('button').contains('Admin').click()
-        cy.contains('Project configuration: Questionnaire').should('be.visible')
-    })
+    it.only('Edit question - change title, support notes and  ', () => {
+        cy.get('[id^=question-]').then(questions => {
+            var questionsCount = Cypress.$(questions).length
 
+            const questionNo = faker.datatype.number({ min: 1, max: questionsCount })
+            const newTitle = 'This is the new title'
+            const newSupportNotes = 'These are the new support notes!!!'
+            cy.get('button').contains('Admin').click()
+            cy.contains('Project configuration: Questionnaire').should('be.visible')
+            cy.getByDataTestid('edit-question-' + questionNo).click()
+            cy.getByDataTestid('question-title-' + questionNo).clear()
+            cy.getByDataTestid('question-title-' + questionNo).type(newTitle)
+            cy.getByDataTestid('markdown-editor')
+                .shadow()
+                .within(() => {
+                    cy.get('[id=editor]').replace(newSupportNotes)
+                })
+            const organization = faker.random.arrayElement(Object.keys(Organization))
+            cy.get(`[id=question-${questionNo}]`).within(() => {
+                cy.contains('Organization')
+                    .next()
+                    .click()
+                    .clear()
+                    .type(organization + '{enter}')
+            })
+
+            cy.getByDataTestid('save-question').click()
+            cy.getByDataTestid('question-title-' + questionNo).should('have.text', newTitle)
+            cy.getByDataTestid('organization-' + questionNo).should('have.text', organization)
+            cy.getByDataTestid('question-title-' + questionNo)
+                .parent()
+                .within(() => {
+                    cy.contains(newSupportNotes).should('be.visible')
+                })
+        })
+    })
     it('Navigate to a barrier', () => {
+        cy.get('button').contains('Admin').click()
         cy.get('a').contains('PS1 Containment').click()
         cy.get('h3').contains('Containment').should('be.visible')
     })
