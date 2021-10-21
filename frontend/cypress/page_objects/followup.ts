fc02a5048d5ac7ed78bdbc9e4b67051e04cc7279
petterwildhagen <pewi@equinor.com>
2021-10-21 12:07:27 +0200

    Add Cypress test to edit action on follow-up->action table

--- frontend/cypress/page_objects/followup.ts
+++ frontend/cypress/page_objects/followup.ts
@@ -2,4 +2,7 @@ export default class FollowUpTabs {
     workshopSummary = () => {
         return cy.get('#fixed-tablist').contains('Workshop Summary')
     }
+    actions = () => {
+        return cy.get('#fixed-tablist').contains('Actions')
+    }
 }
