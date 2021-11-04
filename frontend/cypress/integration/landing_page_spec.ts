a5537f07fe83b8992042c6c5d83978c2424fe971
petterwildhagen <pewi@equinor.com>
2021-11-04 14:43:40 +0100

    Add test hidden evals are displayed in hidden tab (#850)
    Refactored all tests. 
    Added tests for admin role where admin can see voided evaluations under hidden evaluations tab
    Users without admin role cannot see hidden evaluations tab.

--- frontend/cypress/integration/landing_page_spec.ts
+++ frontend/cypress/integration/landing_page_spec.ts
@@ -1,5 +1,5 @@
 import * as faker from 'faker'
-import { Progression, Role } from '../../src/api/models'
+import { Progression, Role, Status } from '../../src/api/models'
 import { EvaluationSeed } from '../support/testsetup/evaluation_seed'
 import { getUsers } from '../support/mock/external/users'
 import { EvaluationPage } from '../page_objects/evaluation'
@@ -11,194 +11,196 @@ describe('Landing page', () => {
     const users = getUsers(3)
     const roles = [Role.Facilitator, Role.OrganizationLead, Role.Participant]
     const user = users[2]
+    const adminUser = getUsers(6)[5]
+    const selectedProject = fusionProject1
+    const otherProject = fusionProject4
 
-    const evaluationIAmIn = new EvaluationSeed({
+    const myActiveEvaluationInProject = new EvaluationSeed({
         progression: faker.random.arrayElement(Object.values(Progression)),
         users,
         roles,
-        fusionProjectId: fusionProject1.id,
+        fusionProjectId: selectedProject.id,
         namePrefix: 'myEval',
     })
-    evaluationIAmIn
+    myActiveEvaluationInProject
         .addAction(
-            evaluationIAmIn.createAction({
+            myActiveEvaluationInProject.createAction({
                 completed: false,
-                assignedTo: evaluationIAmIn.participants.find(p => p.user === user),
+                assignedTo: myActiveEvaluationInProject.participants.find(p => p.user === user),
                 title: 'My action' + faker.lorem.words(),
                 description: 'My action' + faker.lorem.words(),
             })
         )
         .addAction(
-            evaluationIAmIn.createAction({
+            myActiveEvaluationInProject.createAction({
                 completed: false,
-                assignedTo: evaluationIAmIn.participants.find(p => p.user !== user),
+                assignedTo: myActiveEvaluationInProject.participants.find(p => p.user !== user),
                 title: 'Not my action' + faker.lorem.words(),
                 description: 'Not my action' + faker.lorem.words(),
             })
         )
         .addAction(
-            evaluationIAmIn.createAction({
+            myActiveEvaluationInProject.createAction({
                 isVoided: true,
                 completed: false,
-                assignedTo: evaluationIAmIn.participants.find(p => p.user === user),
+                assignedTo: myActiveEvaluationInProject.participants.find(p => p.user === user),
                 title: 'My voided action' + faker.lorem.words(),
                 description: 'My voided action' + faker.lorem.words(),
             })
         )
         .addAction(
-            evaluationIAmIn.createAction({
+            myActiveEvaluationInProject.createAction({
                 isVoided: false,
                 completed: true,
-                assignedTo: evaluationIAmIn.participants.find(p => p.user === user),
+                assignedTo: myActiveEvaluationInProject.participants.find(p => p.user === user),
                 title: 'My completed action' + faker.lorem.words(),
                 description: 'My completed action' + faker.lorem.words(),
             })
         )
 
-    const evaluationIAmNotIn = new EvaluationSeed({
+    const notMyActiveEvaluationInProject = new EvaluationSeed({
         progression: faker.random.arrayElement(Object.values(Progression)),
         users: users.slice(0, 2),
         roles: roles.slice(0, 2),
-        fusionProjectId: fusionProject1.id,
+        fusionProjectId: selectedProject.id,
         namePrefix: 'notMyEval',
     })
-    const evaluationNotInProject = new EvaluationSeed({
+    const myActiveEvaluationNotInProject = new EvaluationSeed({
         progression: faker.random.arrayElement(Object.values(Progression)),
         users: users,
         roles: roles,
-        fusionProjectId: fusionProject4.id,
+        fusionProjectId: otherProject.id,
         namePrefix: 'notProjectEval',
     })
-
-    before(() => {
-        evaluationIAmIn.plant()
-        evaluationIAmNotIn.plant()
-        evaluationNotInProject.plant()
+    const myHiddenEvaluationInProject = new EvaluationSeed({
+        progression: faker.random.arrayElement(Object.values(Progression).filter(p => p !== Progression.Nomination)),
+        users: users,
+        roles: roles,
+        fusionProjectId: selectedProject.id,
+        namePrefix: 'hiddenEvalInProject',
+        status: Status.Voided,
+    })
+    const notMyHiddenEvaluationNotInProject = new EvaluationSeed({
+        progression: faker.random.arrayElement(Object.values(Progression).filter(p => p !== Progression.Nomination)),
+        users: users.slice(0, 2),
+        roles: roles.slice(0, 2),
+        fusionProjectId: otherProject.id,
+        namePrefix: 'hiddenEvalNotInProject',
+        status: Status.Voided,
     })
 
-    beforeEach(() => {
-        cy.visitProject(user, fusionProject1.id)
+    before('Plant (load into DB) all evaluations', () => {
+        myActiveEvaluationInProject.plant()
+        notMyActiveEvaluationInProject.plant()
+        myActiveEvaluationNotInProject.plant()
+        myHiddenEvaluationInProject.plant()
+        notMyHiddenEvaluationNotInProject.plant()
     })
 
-    context(`Dashboard `, () => {
-        context('My evaluations (only my evaluations are listed)', () => {
-            const testdata = [
-                {
-                    eval: evaluationIAmIn,
-                    isListed: true,
-                },
-                {
-                    eval: evaluationIAmNotIn,
-                    isListed: false,
-                },
-                {
-                    eval: evaluationNotInProject,
-                    isListed: true,
-                },
-            ]
+    const evaluations = [
+        { evaluation: myActiveEvaluationInProject, mine: true, inProject: true, hidden: false },
+        { evaluation: notMyActiveEvaluationInProject, mine: false, inProject: true, hidden: false },
+        { evaluation: myActiveEvaluationNotInProject, mine: true, inProject: false, hidden: false },
+        { evaluation: myHiddenEvaluationInProject, mine: true, inProject: false, hidden: true },
+        { evaluation: notMyHiddenEvaluationNotInProject, mine: false, inProject: false, hidden: true },
+    ]
 
-            testdata.forEach(t => {
-                it(`Evaluation of ${t.eval.fusionProjectId === evaluationIAmIn.fusionProjectId ? 'selected' : ' different'} project (id=${
-                    t.eval.fusionProjectId
-                }) that user ${t.isListed ? 'participates in ' : 'does not participate in '} is ${
-                    t.isListed ? '' : 'not'
-                } listed under My evaluations`, () => {
-                    const evalName = t.eval.name
-                    cy.get(`[data-testid=project-table]`).within(() => {
-                        t.isListed ? cy.contains(evalName).should('exist') : cy.contains(evalName).should('not.exist')
+    context(`Dashboard - non admin user `, () => {
+        beforeEach('Log in as user', () => {
+            cy.visitProject(user, fusionProject1.id)
+        })
+        context('My evaluations are listed regardless of status and project', () => {
+            it(`All evaluations of user are listed under my evaluations - irrespective of status and project`, () => {
+                cy.get(`[data-testid=project-table]`).within(() => {
+                    evaluations.forEach(t => {
+                        const evalName = t.evaluation.name
+                        t.mine ? cy.contains(evalName).should('exist') : cy.contains(evalName).should('not.exist')
                     })
                 })
             })
 
             it('User can open own evaluation of selected project', () => {
-                const myEvalName = evaluationIAmIn.name
+                const myEvalName = myActiveEvaluationInProject.name
                 cy.contains(myEvalName).click()
                 const evaluationPage = new EvaluationPage()
-                evaluationPage.progressionStepLink(evaluationIAmIn.progression).should('be.visible')
+                evaluationPage.progressionStepLink(myActiveEvaluationInProject.progression).should('be.visible')
+            })
+        })
+
+        context('Project evaluations (only active evaluation of selected project are listed)', () => {
+            it('Only active evaluations in selected project are listed under Project evaluations', () => {
+                cy.contains('Project evaluations').click()
+                cy.get(`[data-testid=project-table]`).within(() => {
+                    evaluations.forEach(t => {
+                        const evalName = t.evaluation.name
+                        t.inProject ? cy.contains(evalName).should('exist') : cy.contains(evalName).should('not.exist')
+                    })
+                })
+            })
+
+            it('User can open evaluation user is not part of', () => {
+                cy.contains('Project evaluations').click()
+                const notMyEvalName = notMyActiveEvaluationInProject.name
+                cy.contains(notMyEvalName).click()
+                const evaluationPage = new EvaluationPage()
+                evaluationPage.progressionStepLink(notMyActiveEvaluationInProject.progression).should('be.visible')
             })
         })
     })
-
-    context('Project evaluations (only evaluation of selected project are listed)', () => {
-        const testdata = [
-            {
-                eval: evaluationIAmIn,
-                userIsIn: true,
-                isListed: true,
-            },
-            {
-                eval: evaluationIAmNotIn,
-                userIsIn: false,
-                isListed: true,
-            },
-            {
-                eval: evaluationNotInProject,
-                userIsIn: true,
-                isListed: false,
-            },
-        ]
-
-        testdata.forEach(t => {
-            it(`Evaluation of ${
-                t.eval.fusionProjectId === evaluationIAmIn.fusionProjectId ? 'selected' : ' different'
-            } project the user is ${t.userIsIn ? '' : 'not '} in is ${t.isListed ? '' : 'not'} listed under Project evaluations`, () => {
-                cy.contains('Project evaluations').click()
-                const evalName = t.eval.name
-                cy.get(`[data-testid=project-table]`).within(() => {
-                    t.isListed ? cy.contains(evalName).should('exist') : cy.contains(evalName).should('not.exist')
-                })
-            })
+    context(`Dashboard - admin`, () => {
+        it('User with no admin role cannot see hidden evaluations', () => {
+            cy.visitProject(user, fusionProject1.id)
+            cy.contains('Project evaluations').should('exist')
+            cy.get('[role="button"').contains('Hidden evaluations').should('not.exist')
         })
 
-        it('User can open evaluation user is not part of', () => {
-            cy.contains('Project evaluations').click()
-            const notMyEvalName = evaluationIAmNotIn.name
-            cy.contains(notMyEvalName).click()
-            const evaluationPage = new EvaluationPage()
-            evaluationPage.progressionStepLink(evaluationIAmNotIn.progression).should('be.visible')
+        it('Hidden evaluations accross all projects are listed under Hidden evaluations tab', () => {
+            cy.visitProject(adminUser, fusionProject1.id)
+            cy.get('[role="button"').contains('Hidden evaluations').click()
+            evaluations.forEach(t => {
+                const evalName = t.evaluation.name
+                t.hidden ? cy.contains(evalName).should('exist') : cy.contains(evalName).should('not.exist')
+            })
         })
     })
 
     context('Actions', () => {
+        beforeEach('Log in as user, go to actions table', () => {
+            cy.visitProject(user, fusionProject1.id)
+            cy.get('button').contains('Actions').click()
+        })
+
         context('Action table', () => {
             const actionTable = new ActionTable()
             it(`Action assigned to user is listed
-            voided/cancelled actions assigned to user are not listed`, () => {
-                cy.get('button').contains('Actions').click()
+            voided/cancelled actions assigned to user are not listed
+            actions not assigned to user are not listed`, () => {
                 actionTable.table().within(() => {
-                    cy.contains(evaluationIAmIn.actions.find(a => a.assignedTo.user === user)!.title).should('exist')
-                })
-                actionTable.table().within(() => {
-                    cy.contains(evaluationIAmIn.actions.find(a => a.assignedTo.user === user && a.isVoided === true)!.title).should(
-                        'not.exist'
-                    )
-                })
-            })
-            it('Action not assigned to user is not listed', () => {
-                cy.get('button').contains('Actions').click()
-                actionTable.table().within(() => {
-                    cy.contains(evaluationIAmIn.actions.find(a => a.assignedTo.user !== user)!.title).should('not.exist')
+                    cy.contains(myActiveEvaluationInProject.actions.find(a => a.assignedTo.user === user)!.title).should('exist')
+                    cy.contains(
+                        myActiveEvaluationInProject.actions.find(a => a.assignedTo.user === user && a.isVoided === true)!.title
+                    ).should('not.exist')
+                    cy.contains(myActiveEvaluationInProject.actions.find(a => a.assignedTo.user !== user)!.title).should('not.exist')
                 })
             })
+
             it('Verify user can edit his own actions', () => {
-                cy.get('button').contains('Actions').click()
                 const editActionDialog = new EditActionDialog()
                 actionTable.table().should('be.visible')
-                actionTable.action(evaluationIAmIn.actions[0].title).click({ force: true })
+                actionTable.action(myActiveEvaluationInProject.actions[0].title).click({ force: true })
                 editActionDialog.body().should('be.visible')
             })
         })
     })
 
     context('Page navigation', () => {
-        it('User can navigate to Actions tab from Dashboard', () => {
-            cy.get('button').contains('Actions').click()
-            cy.get(`[data-testid=action-table]`).should('be.visible')
+        beforeEach('Log in as user', () => {
+            cy.visitProject(user, fusionProject1.id)
         })
         it('User can navigate to Project evaluations from My evaluations', () => {
             cy.contains('Project evaluations').click()
             cy.get(`[data-testid=project-table]`).should('exist')
-            const notMyEvalName = evaluationIAmNotIn.name
+            const notMyEvalName = notMyActiveEvaluationInProject.name
             cy.contains(notMyEvalName).should('exist')
         })
     })
