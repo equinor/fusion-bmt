bc5d3314795bd81c08b6521ffeea4114699741ac
petterwildhagen <pewi@equinor.com>
2021-12-02 13:07:14 +0100

    View answers follow up and workshop summary (#926)
    * Create Cypress test for viewing answer on stages workshop and follow-up

--- frontend/cypress/integration/view_answers_spec.ts
+++ frontend/cypress/integration/view_answers_spec.ts
@@ -1,89 +1,202 @@
 import { Progression, Role } from '../../src/api/models'
 import { EvaluationSeed } from '../support/testsetup/evaluation_seed'
-import { getUsers, User } from '../support/mock/external/users'
+import { getUsers } from '../support/mock/external/users'
 import { fusionProject1 } from '../support/mock/external/projects'
 import { Answer } from '../support/testsetup/mocks'
 import { EvaluationPage } from '../page_objects/evaluation'
+import * as faker from 'faker'
 
-describe('Viewing answers on stage PREPARATION', () => {
+describe('Viewing answers', () => {
     const users = getUsers(4)
     const roles = [Role.Facilitator, Role.OrganizationLead, Role.Participant, Role.Participant]
     const evaluationPage = new EvaluationPage()
-    const evalWithIndividualAnswers = new EvaluationSeed({
-        progression: Progression.Preparation,
+    const evalWithIndividualAndPreparationAndWorkshopAnswers = new EvaluationSeed({
+        progression: faker.random.arrayElement(
+            Object.values(Progression).filter(p => p !== Progression.Finished && p !== Progression.Nomination)
+        ),
         users,
         roles,
         fusionProjectId: fusionProject1.id,
         namePrefix: 'someAnswers',
     })
-    const firstParticipant = evalWithIndividualAnswers.participants.filter(p => p.role === Role.Participant)[0]
-    const secondParticipant = evalWithIndividualAnswers.participants.filter(p => p.role === Role.Participant)[1]
-    const lead = evalWithIndividualAnswers.participants.find(p => p.role === Role.OrganizationLead)!
-    const facilitator = evalWithIndividualAnswers.participants.find(p => p.role === Role.Facilitator)!
+    const firstParticipant = evalWithIndividualAndPreparationAndWorkshopAnswers.participants.filter(p => p.role === Role.Participant)[0]
+    const secondParticipant = evalWithIndividualAndPreparationAndWorkshopAnswers.participants.filter(p => p.role === Role.Participant)[1]
+    const lead = evalWithIndividualAndPreparationAndWorkshopAnswers.participants.find(p => p.role === Role.OrganizationLead)!
+    const facilitator = evalWithIndividualAndPreparationAndWorkshopAnswers.participants.find(p => p.role === Role.Facilitator)!
     const questionOrder = 1
     const firstParticipantAnswerText = 'First Answer by first participant'
     const secondParticipantAnswerText = 'Second Answer by second participant'
     const facilitatorAnswerText = 'Facilitator answer text'
     const leadAnswerText = 'Lead answer text'
-    evalWithIndividualAnswers
-        .addAnswer(
-            new Answer({
-                questionOrder: questionOrder,
-                answeredBy: firstParticipant,
-                progression: Progression.Individual,
-                text: firstParticipantAnswerText,
-            })
-        )
-        .addAnswer(
-            new Answer({
-                questionOrder: questionOrder,
-                answeredBy: secondParticipant,
-                progression: Progression.Individual,
-                text: secondParticipantAnswerText,
-            })
-        )
-        .addAnswer(
-            new Answer({
-                questionOrder: questionOrder,
-                answeredBy: facilitator,
-                progression: Progression.Individual,
-                text: facilitatorAnswerText,
-            })
-        )
-        .addAnswer(
-            new Answer({
-                questionOrder: questionOrder,
-                answeredBy: lead,
-                progression: Progression.Individual,
-                text: leadAnswerText,
-            })
-        )
-    before('Load evaluation with answers from all two participant, one lead and one facilitator', () => {
-        evalWithIndividualAnswers.plant()
+    const facilitatorPrepAnswerText = 'Facilitator prep answer text'
+    const leadPrepAnswerText = 'Lead prep answer text'
+    const facilitatorWorkshopAnswerText = 'Facilitator workshop answer text'
+
+    const firstParticipantAnswer = new Answer({
+        questionOrder: questionOrder,
+        answeredBy: firstParticipant,
+        progression: Progression.Individual,
+        text: firstParticipantAnswerText,
     })
-    it('Participant can only see his/her own answer', () => {
-        cy.visitProgression(Progression.Preparation, evalWithIndividualAnswers.evaluationId, firstParticipant.user, fusionProject1.id)
-        evaluationPage.clickViewAnswers(questionOrder)
-        cy.contains(firstParticipantAnswerText).should('be.visible')
-        cy.contains(secondParticipantAnswerText).should('not.exist')
-        cy.contains(facilitatorAnswerText).should('not.exist')
-        cy.contains(leadAnswerText).should('not.exist')
+    const secondParticipantAnswer = new Answer({
+        questionOrder: questionOrder,
+        answeredBy: secondParticipant,
+        progression: Progression.Individual,
+        text: secondParticipantAnswerText,
+    })
+    const facilitatorAnswer = new Answer({
+        questionOrder: questionOrder,
+        answeredBy: facilitator,
+        progression: Progression.Individual,
+        text: facilitatorAnswerText,
+    })
+    const leadAnswer = new Answer({
+        questionOrder: questionOrder,
+        answeredBy: lead,
+        progression: Progression.Individual,
+        text: leadAnswerText,
+    })
+    const facilitatorPrepAnswer = new Answer({
+        questionOrder: questionOrder,
+        answeredBy: facilitator,
+        progression: Progression.Preparation,
+        text: facilitatorPrepAnswerText,
+    })
+    const leadPrepAnswer = new Answer({
+        questionOrder: questionOrder,
+        answeredBy: lead,
+        progression: Progression.Preparation,
+        text: leadPrepAnswerText,
+    })
+    const facilitatorWorkshopAnswer = new Answer({
+        questionOrder: questionOrder,
+        answeredBy: facilitator,
+        progression: Progression.Workshop,
+        text: facilitatorWorkshopAnswerText,
     })
 
-    const LeadFacilitarorRoles = [Role.Facilitator, Role.OrganizationLead]
-    LeadFacilitarorRoles.forEach(role => {
-        it(`${role} can see answers regardless of role`, () => {
+    before(
+        `Load evaluation with individual answers from all: two participants, one lead and one facilitator
+    and preparation answers, one from lead and one from facilitator
+    and one workshop answer from facilitator`,
+        () => {
+            evalWithIndividualAndPreparationAndWorkshopAnswers
+                .addAnswer(firstParticipantAnswer)
+                .addAnswer(secondParticipantAnswer)
+                .addAnswer(facilitatorAnswer)
+                .addAnswer(leadAnswer)
+                .addAnswer(facilitatorPrepAnswer)
+                .addAnswer(leadPrepAnswer)
+                .addAnswer(facilitatorWorkshopAnswer)
+            evalWithIndividualAndPreparationAndWorkshopAnswers.plant()
+        }
+    )
+
+    const leadFacilitarorRoles = [Role.Facilitator, Role.OrganizationLead]
+    context('Viewing answers on stage PREPARATION', () => {
+        it('Participant can only see his/her own individual answer', () => {
             cy.visitProgression(
                 Progression.Preparation,
-                evalWithIndividualAnswers.evaluationId,
-                evalWithIndividualAnswers.participants.find(p => p.role === role)!.user,
+                evalWithIndividualAndPreparationAndWorkshopAnswers.evaluationId,
+                firstParticipant.user,
                 fusionProject1.id
             )
             evaluationPage.clickViewAnswers(questionOrder)
             cy.contains(firstParticipantAnswerText).should('be.visible')
-            cy.contains(secondParticipantAnswerText).should('be.visible')
-            cy.contains(facilitatorAnswerText).should('be.visible')
-            cy.contains(leadAnswerText).should('be.visible')
+            cy.contains(secondParticipantAnswerText).should('not.exist')
+            cy.contains(facilitatorAnswerText).should('not.exist')
+            cy.contains(leadAnswerText).should('not.exist')
+        })
+
+        leadFacilitarorRoles.forEach(role => {
+            it(`${role} can see all individual answers regardless of role`, () => {
+                cy.visitProgression(
+                    Progression.Preparation,
+                    evalWithIndividualAndPreparationAndWorkshopAnswers.evaluationId,
+                    evalWithIndividualAndPreparationAndWorkshopAnswers.participants.find(p => p.role === role)!.user,
+                    fusionProject1.id
+                )
+                evaluationPage.clickViewAnswers(questionOrder)
+                cy.contains(firstParticipantAnswerText).should('be.visible')
+                cy.contains(secondParticipantAnswerText).should('be.visible')
+                cy.contains(facilitatorAnswerText).should('be.visible')
+                cy.contains(leadAnswerText).should('be.visible')
+            })
+        })
+    })
+    context('Viewing answers on stage Workshop Summary', () => {
+        it(`Participant can only see his/her own individual answer 
+            and preparation answers from lead and facilitator`, () => {
+            cy.visitProgression(
+                Progression.Workshop,
+                evalWithIndividualAndPreparationAndWorkshopAnswers.evaluationId,
+                firstParticipant.user,
+                fusionProject1.id
+            )
+            evaluationPage.clickViewAnswers(questionOrder)
+            cy.contains(firstParticipantAnswerText).should('be.visible')
+            cy.contains(secondParticipantAnswerText).should('not.exist')
+            cy.contains(facilitatorAnswerText).should('not.exist')
+            cy.contains(leadAnswerText).should('not.exist')
+            cy.contains(facilitatorPrepAnswerText).should('be.visible')
+            cy.contains(leadPrepAnswerText).should('be.visible')
+        })
+        leadFacilitarorRoles.forEach(role => {
+            it(`${role} can see all individual and preparation answers`, () => {
+                const user = evalWithIndividualAndPreparationAndWorkshopAnswers.participants.find(p => p.role === role)!.user
+                cy.visitProgression(
+                    Progression.Workshop,
+                    evalWithIndividualAndPreparationAndWorkshopAnswers.evaluationId,
+                    user,
+                    fusionProject1.id
+                )
+                evaluationPage.clickViewAnswers(questionOrder)
+                evaluationPage.verifyAnswerVisible('Individual', firstParticipant.user.name, firstParticipantAnswerText)
+                evaluationPage.verifyAnswerVisible('Individual', secondParticipant.user.name, secondParticipantAnswerText)
+                evaluationPage.verifyAnswerVisible('Individual', lead.user.name, leadAnswerText)
+                evaluationPage.verifyAnswerVisible('Individual', facilitator.user.name, facilitatorAnswerText)
+                evaluationPage.verifyAnswerVisible('Preparation', facilitator.user.name, facilitatorPrepAnswerText)
+                evaluationPage.verifyAnswerVisible('Preparation', lead.user.name, leadPrepAnswerText)
+            })
+        })
+    })
+    context('Viewing answers on stage Follow-up', () => {
+        it(`Participant can only see his/her own individual answer 
+        and preparation answers from lead and facilitator
+        and workshop answer from facilitator`, () => {
+            cy.visitProgression(
+                Progression.FollowUp,
+                evalWithIndividualAndPreparationAndWorkshopAnswers.evaluationId,
+                firstParticipant.user,
+                fusionProject1.id
+            )
+            evaluationPage.clickViewAnswers(questionOrder)
+            cy.contains(firstParticipantAnswerText).should('be.visible')
+            cy.contains(secondParticipantAnswerText).should('not.exist')
+            cy.contains(facilitatorAnswerText).should('not.exist')
+            cy.contains(leadAnswerText).should('not.exist')
+            cy.contains(facilitatorPrepAnswerText).should('be.visible')
+            cy.contains(leadPrepAnswerText).should('be.visible')
+            cy.contains(facilitatorWorkshopAnswerText).should('be.visible')
+        })
+        leadFacilitarorRoles.forEach(role => {
+            it(`${role} can see all individual, preparation and workshop answers`, () => {
+                const user = evalWithIndividualAndPreparationAndWorkshopAnswers.participants.find(p => p.role === role)!.user
+                cy.visitProgression(
+                    Progression.FollowUp,
+                    evalWithIndividualAndPreparationAndWorkshopAnswers.evaluationId,
+                    user,
+                    fusionProject1.id
+                )
+                evaluationPage.clickViewAnswers(questionOrder)
+                evaluationPage.verifyAnswerVisible('Individual', firstParticipant.user.name, firstParticipantAnswerText)
+                evaluationPage.verifyAnswerVisible('Individual', secondParticipant.user.name, secondParticipantAnswerText)
+                evaluationPage.verifyAnswerVisible('Individual', lead.user.name, leadAnswerText)
+                evaluationPage.verifyAnswerVisible('Individual', facilitator.user.name, facilitatorAnswerText)
+                evaluationPage.verifyAnswerVisible('Preparation', facilitator.user.name, facilitatorPrepAnswerText)
+                evaluationPage.verifyAnswerVisible('Preparation', lead.user.name, leadPrepAnswerText)
+                evaluationPage.verifyAnswerVisible('Workshop', 'Facilitators', facilitatorWorkshopAnswerText)
+            })
         })
     })
 })
--- frontend/cypress/page_objects/evaluation.ts
+++ frontend/cypress/page_objects/evaluation.ts
@@ -37,6 +37,12 @@ export class EvaluationPage {
             cy.getByDataTestid('view-answers').click()
         })
     }
+
+    verifyAnswerVisible = (stage: string, user: string, answer: string) => {
+        cy.contains('h5', stage).nextUntil().contains(user).scrollIntoView()
+        cy.contains('h5', stage).nextUntil().contains(user).should('be.visible')
+        cy.contains('h5', stage).nextUntil().contains(user).next().contains(answer).should('be.visible')
+    }
 }
 
 export enum Barrier {
