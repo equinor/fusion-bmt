2a36f4044b2cd722bea467c1b400f311c188ea0e
Arian <89781510+arian-garshi@users.noreply.github.com>
2024-01-09 13:19:13 +0100

    fix: rmv fusion table dependency

--- frontend/src/components/ProgressSummary.tsx
+++ frontend/src/components/ProgressSummary.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Evaluation, Organization, Participant, Question, Progression, Role } from '../api/models'
 import ParticipantCard from './ParticipantCard'
-import { DataTableColumn, DataTable } from '@equinor/fusion-components'
+import { Table } from '@equinor/eds-core-react'
 import { progressionLessThan, ProgressionStatus } from '../utils/ProgressionStatus'
 import { getFilledUserAnswersForProgression } from '../utils/QuestionAndAnswerUtils'
 import { organizationToString } from '../utils/EnumToString'
+import styled from 'styled-components'
+
+const StyledTable = styled(Table)`
+    width: 100%;
+`
 
 interface DataTableItem {
     organization: Organization
@@ -14,67 +19,6 @@ interface DataTableItem {
     completed: boolean
     rowIdentifier: string
 }
-
-interface DataTableRowProps {
-    item: DataTableItem
-    rowIndex: number
-}
-
-const ParticipantCardRenderer: React.FC<DataTableRowProps> = ({ item }) => <ParticipantCard participant={item.participant} />
-
-const ProgressOrganisationRenderer: React.FC<DataTableRowProps> = ({ item }) => {
-    return <>{item.progressOrg}%</>
-}
-
-const ProgressAllRenderer: React.FC<DataTableRowProps> = ({ item }) => {
-    return <>{item.progressAll}%</>
-}
-
-const OrganizationRenderer: React.FC<DataTableRowProps> = ({ item }) => {
-    return <>{organizationToString(item.organization)}</>
-}
-
-const CompletedRenderer: React.FC<DataTableRowProps> = ({ item }) => {
-    return <>{item.completed ? 'Yes' : 'No'}</>
-}
-
-const columns: DataTableColumn<DataTableItem>[] = [
-    {
-        key: 'person',
-        accessor: 'participant',
-        label: 'Details',
-        sortable: false,
-        component: ParticipantCardRenderer,
-    },
-    {
-        key: 'organization',
-        accessor: 'organization',
-        label: 'Organization',
-        sortable: false,
-        component: OrganizationRenderer,
-    },
-    {
-        key: 'progressOrg',
-        accessor: 'organization',
-        label: 'Progress organisation questions',
-        sortable: false,
-        component: ProgressOrganisationRenderer,
-    },
-    {
-        key: 'progressAll',
-        accessor: 'organization',
-        label: 'Progress all questions',
-        sortable: false,
-        component: ProgressAllRenderer,
-    },
-    {
-        key: 'completed',
-        accessor: 'completed',
-        label: 'Completed',
-        sortable: false,
-        component: CompletedRenderer,
-    },
-]
 
 interface ProgressSummaryProps {
     evaluation: Evaluation
@@ -117,9 +61,30 @@ const ProgressSummary = ({ evaluation, viewProgression, allowedRoles }: Progress
         })
 
     return (
-        <>
-            <DataTable columns={columns} data={data} isFetching={false} rowIdentifier={'rowIdentifier'} />
-        </>
+        <StyledTable>
+            <Table.Head>
+                <Table.Row>
+                    <Table.Cell>Details</Table.Cell>
+                    <Table.Cell>Organization</Table.Cell>
+                    <Table.Cell>Progress organisation questions</Table.Cell>
+                    <Table.Cell>Progress all questions</Table.Cell>
+                    <Table.Cell>Completed</Table.Cell>
+                </Table.Row>
+            </Table.Head>
+            <Table.Body>
+                {data.map((item, index) => (
+                    <Table.Row key={index}>
+                        <Table.Cell>
+                            <ParticipantCard participant={item.participant} />
+                        </Table.Cell>
+                        <Table.Cell>{organizationToString(item.organization)}</Table.Cell>
+                        <Table.Cell>{item.progressOrg}%</Table.Cell>
+                        <Table.Cell>{item.progressAll}%</Table.Cell>
+                        <Table.Cell>{item.completed ? 'Yes' : 'No'}</Table.Cell>
+                    </Table.Row>
+                ))}
+            </Table.Body>
+        </StyledTable>
     )
 }
 
