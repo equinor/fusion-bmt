cf16204b36afa3ccbc5475f93752c19c906ce14d
Tord Austad <toaus@equinor.com>
2024-04-10 13:51:52 +0200

    add migration files

--- backend/api/Migrations/20240410114952_EvaluationActivityIndicator.Designer.cs
+++ backend/api/Migrations/20240410114952_EvaluationActivityIndicator.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using api.Context;
 
@@ -11,9 +12,10 @@ using api.Context;
 namespace api.Migrations
 {
     [DbContext(typeof(BmtDbContext))]
-    partial class BmtDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240410114952_EvaluationActivityIndicator")]
+    partial class EvaluationActivityIndicator
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
