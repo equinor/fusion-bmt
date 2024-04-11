9fde8ea4a18315ae19a4279ffcc7609985b9489a
Tord Austad <toaus@equinor.com>
2024-04-11 09:29:52 +0200

    fix: Fix setting API url

--- frontend/src/environmentConfig.ts
+++ frontend/src/environmentConfig.ts
@@ -2,47 +2,33 @@ export const resolveConfiguration = (env: string) => {
     switch (env) {
     case "FPRD":
         return {
-            REACT_APP_API_BASE_URL:
-                    "https://backend-dcd-prod.radix.equinor.com",
-            BACKEND_APP_SCOPE: [
-                "api://81bd7b7f-4096-4c4f-b0c2-ebef7d05c0e6/ConceptApp.CreateEditProjects",
-            ],
-            APP_ID: "81bd7b7f-4096-4c4f-b0c2-ebef7d05c0e6",
+            REACT_APP_API_BASE_URL: "https://backend-acr-fusion-bmt-prod.radix.equinor.com",
+            BACKEND_APP_SCOPE: ["api://8829d4ca-93e8-499a-8ce1-bc0ef4840176/user_impersonation"],
+            APP_ID: "8829d4ca-93e8-499a-8ce1-bc0ef4840176",
         }
     case "FQA":
         return {
-            REACT_APP_API_BASE_URL:
-                    "https://backend-dcd-qa.radix.equinor.com",
-            BACKEND_APP_SCOPE: [
-                "api://151950a5-f886-47cd-b361-afb81e75c345/Project.ReadWrite",
-            ],
-            APP_ID: "9b125a0c-4907-43b9-8db2-ff405d6b0524",
+            REACT_APP_API_BASE_URL: "https://backend-acr-fusion-bmt-qa.radix.equinor.com",
+            BACKEND_APP_SCOPE: ["api://8829d4ca-93e8-499a-8ce1-bc0ef4840176/user_impersonation"],
+            APP_ID: "8829d4ca-93e8-499a-8ce1-bc0ef4840176",
         }
     case "CI":
         return {
-            REACT_APP_API_BASE_URL:
-                    "https://backend-dcd-dev.radix.equinor.com",
-            BACKEND_APP_SCOPE: [
-                "api://151950a5-f886-47cd-b361-afb81e75c345/Project.ReadWrite",
-            ],
-            APP_ID: "9b125a0c-4907-43b9-8db2-ff405d6b0524",
+            REACT_APP_API_BASE_URL: "https://backend-acr-fusion-bmt-dev.radix.equinor.com",
+            BACKEND_APP_SCOPE: ["api://8829d4ca-93e8-499a-8ce1-bc0ef4840176/user_impersonation"],
+            APP_ID: "8829d4ca-93e8-499a-8ce1-bc0ef4840176",
         }
     case "PR":
         return {
-            REACT_APP_API_BASE_URL:
-                    "https://backend-dcd-dev.radix.equinor.com",
-            BACKEND_APP_SCOPE: [
-                "api://151950a5-f886-47cd-b361-afb81e75c345/Project.ReadWrite",
-            ],
-            APP_ID: "9b125a0c-4907-43b9-8db2-ff405d6b0524",
+            REACT_APP_API_BASE_URL: "https://backend-fusion-bmt-pr.radix.equinor.com",
+            BACKEND_APP_SCOPE: ["api://8829d4ca-93e8-499a-8ce1-bc0ef4840176/user_impersonation"],
+            APP_ID: "8829d4ca-93e8-499a-8ce1-bc0ef4840176",
         }
     case "dev":
         return {
             REACT_APP_API_BASE_URL: "http://localhost:5000",
-            BACKEND_APP_SCOPE: [
-                "api://151950a5-f886-47cd-b361-afb81e75c345/Project.ReadWrite",
-            ],
-            APP_ID: "9b125a0c-4907-43b9-8db2-ff405d6b0524",
+            BACKEND_APP_SCOPE: ["api://8829d4ca-93e8-499a-8ce1-bc0ef4840176/user_impersonation"],
+            APP_ID: "8829d4ca-93e8-499a-8ce1-bc0ef4840176",
         }
     default:
         throw new Error(`Unknown env '${env}'`)
