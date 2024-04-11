42abe8603ce816d729638652497b40c5704298cb
Arian <89781510+arian-garshi@users.noreply.github.com>
2024-04-11 09:31:19 +0200

    feat: status circles added

--- frontend/src/views/Project/Dashboard/Components/TablesAndTitles.tsx
+++ frontend/src/views/Project/Dashboard/Components/TablesAndTitles.tsx
@@ -1,19 +1,33 @@
 import { Accordion } from '@equinor/eds-core-react'
 import { EvaluationsByProjectMaster } from '../../../../utils/hooks'
 import EvaluationsTable from './EvaluationsTable'
+import { useProject } from '../../../../globals/contexts'
+import { useEffect } from 'react'
+import EvaluationScoreIndicator from '../../../../components/EvaluationScoreIndicator'
 
 interface Props {
     evaluationsWithProjectMasterTitle: EvaluationsByProjectMaster
 }
 
 const TablesAndTitles = ({ evaluationsWithProjectMasterTitle }: Props) => {
+    const project = useProject()
+
+    useEffect(() => {
+        console.log(evaluationsWithProjectMasterTitle)
+    }, [evaluationsWithProjectMasterTitle])
+
     return (
         <>
             <Accordion headerLevel="h2">
                 {Object.entries(evaluationsWithProjectMasterTitle).map(([projectMasterTitle, evaluations], index) => {
                     return (
                         <Accordion.Item key={index}>
-                            <Accordion.Header>{projectMasterTitle}</Accordion.Header>
+                            <Accordion.Header>
+                                {projectMasterTitle}
+                                <EvaluationScoreIndicator date="2024-02-10T13:04:28.478Z" />
+                                <EvaluationScoreIndicator date="2024-03-10T13:04:28.478Z" />
+                                <EvaluationScoreIndicator date="2024-04-10T13:04:28.478Z" />
+                            </Accordion.Header>
                             <Accordion.Panel>
                                 <EvaluationsTable evaluations={evaluations} />
                             </Accordion.Panel>
