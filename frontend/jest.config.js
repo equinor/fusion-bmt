b00c5cee1167b5f8bd17cd7b6dd7a9e03b30c7ea
Tord Austad <toaus@equinor.com>
2024-04-12 10:38:33 +0200

    fix: fix frontend tests

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,12 +1,16 @@
-module.exports = {
+export default {
     verbose: true,
     testEnvironment: 'jsdom',
+    preset: 'ts-jest',
     transform: {
         '.(js)': 'babel-jest',
         '.(ts|tsx)': 'ts-jest',
     },
+    // moduleNameMapper: {
+    //     '^@equinor/fusion-framework-react/hooks$': './node_modules/@equinor/fusion-framework-react/src/hooks',
+    // },
     testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
     moduleFileExtensions: ['ts', 'tsx', 'js'],
-    transformIgnorePatterns: ['<rootDir>/node_modules/?!(@equinor/fusion)'],
+    // transformIgnorePatterns: ['<rootDir>/node_modules/?!(@equinor/fusion|@equinor/fusion-framework-react/hooks)'],
     setupFilesAfterEnv: ['<rootDir>/setupJest.ts'],
 }
