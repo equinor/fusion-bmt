95f087da0a0453b0cad4fd8ee625af0e7624d73b
Tord Austad <toaus@equinor.com>
2024-04-26 10:50:16 +0200

    fix: Fix setting assignee on action and creating action (#1120)

--- frontend/src/components/Action/CreateForm/ActionCreateSidebar.tsx
+++ frontend/src/components/Action/CreateForm/ActionCreateSidebar.tsx
@@ -34,7 +34,7 @@ const ActionCreateSidebar = ({
     const { personDetailsList, isLoading } = useAllPersonDetailsAsync(possibleAssignees.map(assignee => assignee.azureUniqueId))
 
     return (
-        <SideSheet 
+        <SideSheet
             isOpen={open}
             onClose={onClose}
             minWidth={550}
@@ -42,27 +42,27 @@ const ActionCreateSidebar = ({
             <SideSheet.Title title="Add Action" />
             <SideSheet.SubTitle subTitle="" />
             <SideSheet.Content>
-            {isLoading && (
-                <div style={{ textAlign: 'center' }}>
-                    <CircularProgress />
-                </div>
-            )}
-            {!isLoading && showErrorMessage && (
-                <ErrorBanner message={'Could not save action. ' + genericErrorMessage} onClose={() => setShowErrorMessage(false)} />
-            )}
-            {!isLoading && (
-                <div data-testid="create_action_dialog_body">
-                    <ActionCreateForm
-                        connectedQuestion={connectedQuestion}
-                        possibleAssignees={possibleAssignees}
-                        possibleAssigneesDetails={personDetailsList}
-                        onActionCreate={onActionCreate}
-                        onCancelClick={onClose}
-                        disableCreate={disableCreate}
-                        creatingAction={creatingAction}
-                    />
-                </div>
-            )}
+                {isLoading && (
+                    <div style={{ textAlign: 'center' }}>
+                        <CircularProgress />
+                    </div>
+                )}
+                {!isLoading && showErrorMessage && (
+                    <ErrorBanner message={'Could not save action. ' + genericErrorMessage} onClose={() => setShowErrorMessage(false)} />
+                )}
+                {!isLoading && (
+                    <div data-testid="create_action_dialog_body">
+                        <ActionCreateForm
+                            connectedQuestion={connectedQuestion}
+                            possibleAssignees={possibleAssignees}
+                            possibleAssigneesDetails={personDetailsList}
+                            onActionCreate={onActionCreate}
+                            onCancelClick={onClose}
+                            disableCreate={disableCreate}
+                            creatingAction={creatingAction}
+                        />
+                    </div>
+                )}
             </SideSheet.Content>
         </SideSheet>
     )
