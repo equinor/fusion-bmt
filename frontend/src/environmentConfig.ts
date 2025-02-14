246172edfe11080d1ce9482963ad30d8dc34cb3e
Tord Austad <toaus@equinor.com>
2025-02-14 10:46:02 +0100

    Fix frontend env mapping

--- frontend/src/environmentConfig.ts
+++ frontend/src/environmentConfig.ts
@@ -1,11 +1,7 @@
 export const resolveConfiguration = (env: string) => {
     switch (env) {
+        case "PROD":
         case "FPRD":
-            return {
-                REACT_APP_API_BASE_URL: "https://backend-acr-fusion-bmt-prod.radix.equinor.com",
-                BACKEND_APP_SCOPE: ["api://8829d4ca-93e8-499a-8ce1-bc0ef4840176/user_impersonation"],
-                APP_ID: "8829d4ca-93e8-499a-8ce1-bc0ef4840176",
-            }
         case "PRD":
             return {
                 REACT_APP_API_BASE_URL: "https://backend-acr-fusion-bmt-prod.radix.equinor.com",
@@ -13,11 +9,6 @@ export const resolveConfiguration = (env: string) => {
                 APP_ID: "8829d4ca-93e8-499a-8ce1-bc0ef4840176",
             }
         case "FQA":
-            return {
-                REACT_APP_API_BASE_URL: "https://backend-acr-fusion-bmt-qa.radix.equinor.com",
-                BACKEND_APP_SCOPE: ["api://8829d4ca-93e8-499a-8ce1-bc0ef4840176/user_impersonation"],
-                APP_ID: "8829d4ca-93e8-499a-8ce1-bc0ef4840176",
-            }
         case "QA":
             return {
                 REACT_APP_API_BASE_URL: "https://backend-acr-fusion-bmt-qa.radix.equinor.com",
@@ -27,12 +18,6 @@ export const resolveConfiguration = (env: string) => {
         case "CI":
             return {
                 REACT_APP_API_BASE_URL: "https://backend-acr-fusion-bmt-dev.radix.equinor.com",
-                BACKEND_APP_SCOPE: ["api://8829d4ca-93e8-499a-8ce1-bc0ef4840176/user_impersonation"],
-                APP_ID: "8829d4ca-93e8-499a-8ce1-bc0ef4840176",
-            }
-        case "PR":
-            return {
-                REACT_APP_API_BASE_URL: "https://backend-fusion-bmt-pr.radix.equinor.com",
                 BACKEND_APP_SCOPE: ["api://8829d4ca-93e8-499a-8ce1-bc0ef4840176/user_impersonation"],
                 APP_ID: "8829d4ca-93e8-499a-8ce1-bc0ef4840176",
             }
